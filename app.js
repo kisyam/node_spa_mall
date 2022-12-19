0315db615e41cf3c027264c30c9b3dfd8ef6c4e8
kisyam <how2logout@gmail.com>
2022-12-20 02:47:20 +0900

    221220

--- app.js
+++ app.js
@@ -1,50 +1,19 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-const goodsRouter = require("./routes/goods.js") //goods.js에 있는 라우터를 반환 받음.
-const cartsRouter = require("./routes/carts.js")
-
-const connect = require("./schemas");
-connect();
+const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const User = require("./models/user");
+const Goods = require("./models/goods");
+const Carts = require("./models/cart");
+const authMiddleware = require("./middlewares/auth-middleware");
 
 
-// app.post("/",(req,res)=>{
-//     console.log(req.body);
-
-//     res.send("기본 URI에 POST 메소드가 정상적으로 실행되었습니다.")
-// })
-
-// app.get("/", (req,res) =>{
-//     console.log(req.query);
-
-//     const obj = {
-//         "keykey" : "value 입니다",
-//         "이름입니다" : "이름일까요?"
-//     }
-
-//     res.json(obj);
-// })
-
-// app.get("/:id", (req,res) => {
-//     console.log(req.params);
-
-//     res.send(":id URI 에 정상적으로 반환되었습니다.")
-// })
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-
-
-app.use(express.json());
-
-//localhost:3000/api -> goodRouter
-app.use("/api", [goodsRouter, cartsRouter]); //전역 미들웨어
-
-app.get("/", (req,res) => {
-  res.send("Hello World!");
-})
-
-app.listen(port, () => {
-  console.log(port, '포트로 서버가 열렸어요!');
+mongoose.set("strictQuery", true); //mongoose version 7 error handle
+mongoose.connect("mongodb://localhost/shopping-demo", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+
+const app = express();
+const router = express.Router();
---
+++ models/cart.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const CartSchema = new mongoose.Schema({
+  userId: String,
+  goodsId: String,
+  quantity: Number,
+});
+
+module.exports = mongoose.model("Cart", CartSchema);
